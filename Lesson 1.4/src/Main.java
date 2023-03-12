a29678eab18d5d229a518cdde97909c167235328
PrasolovSam <123651180+PrasolovSam@users.noreply.github.com>
2023-03-12 16:21:21 +0400

    Add files via upload

---
+++ Lesson 1.4/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("***********************");
+        System.out.println("****    Визитка    ****");
+        System.out.println("****  Name: \"Roman\"  ****");
+        System.out.println("* instagram: prasolov *");
+        System.out.println("***********************");
+    }
+}
