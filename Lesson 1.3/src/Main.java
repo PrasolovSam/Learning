d6280a9c760f6b5fe7f8e35552eeee17daa6f963
PrasolovSam <123651180+PrasolovSam@users.noreply.github.com>
2023-03-12 16:18:16 +0400

    Add files via upload

---
+++ Lesson 1.3/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("***********************\n****\tВизитка\t\t****\n****  Name: Roman  ****\n*** Skype: prasolov ***\n***********************");
+    }
+}
